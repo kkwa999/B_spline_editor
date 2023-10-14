c39559b91c74f0febed3f7e9fbcd6c984950cc80
kkwa999 <kkwa999@gmail.com>
2023-10-14 12:43:34 +0900

    Initial commit

---
+++ spline/HW1.js
@@ -0,0 +1,259 @@
+// // Helpers: axesHelper, arrowHelper, gridHelper 에 대한 소개
+// // dat.GUI 를 이용한 user interface
+// import {BSpline} from './BSpline.js';
+// // import {DragControls} from './spline/asset/DragControls.js';
+// // import * as THREE from 'three';
+// // import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+// // import Stats from 'three/addons/libs/stats.module.js';
+
+// global variables
+let scene, renderer, orbit, point, point_geometry;
+let axesHelper, gridHelper;
+let line_visble = true;
+var vertices = [];
+var points = [];
+var lines = [];
+const curves = [];
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+var v_index = 0;
+
+var mouse = new THREE.Vector2();
+var plane = new THREE.Plane();
+var planeNormal = new THREE.Vector3();
+var raycaster = new THREE.Raycaster();
+raycaster.params.Points.threshold = 0.25;
+var planePoint = new THREE.Vector3();
+
+let move_flag = false;
+
+// scene
+scene = new THREE.Scene();
+scene.background = new THREE.Color("black");
+
+// OrthographicCamera: left, right, top, button, near, far
+const camera = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 0.1, 100);
+camera.position.set(0, 0, 4);
+camera.lookAt(0, 0, 0);
+camera.zoom = 20;
+camera.updateProjectionMatrix();
+
+// renderer
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// // orbit control
+// orbit = new OrbitControls( camera, renderer.domElement );
+// orbit.target.set(0, 0, 0);
+// orbit.update();
+
+// axes Helper
+axesHelper = new THREE.AxesHelper(30);
+axesHelper.visible = false;
+scene.add(axesHelper);
+
+// grid helper
+gridHelper = new THREE.GridHelper(50, 30);
+gridHelper.rotation.x = Math.PI / 2;
+gridHelper.position.set(0, 0, -0.001);
+gridHelper.visible = false;
+scene.add( gridHelper );
+
+// GUI
+guiSetup(); 
+
+// Handling resize event
+
+window.addEventListener('resize', resize, true);
+
+function animate(time){
+    //axesHelper.visible = guiControls.axes;
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+function resize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function guiSetup() {
+    const guiControls = new function() {
+        this.axes = false;
+        this.grid = false;
+        this.line = true; 
+        this.zoom_factor = 1; 
+        // this.arrowColor = 0xf5831c; 
+        this.resetView = function() {
+            camera.zoom = 20;
+            ZoomControl.setValue(1.0);
+            camera.updateProjectionMatrix();
+        }
+    }
+    
+    const gui = new dat.GUI();
+    
+    const folderVis = gui.addFolder('Visibility');
+    const axesVisibleControl = folderVis.add(guiControls, 'axes').onChange(function() {
+        axesHelper.visible = guiControls.axes;
+    });
+    const gridVisibleControl = folderVis.add(guiControls, 'grid').onChange(function() {
+        gridHelper.visible = guiControls.grid;
+    });
+    const arrowVisibleControl = folderVis.add(guiControls, 'line').onChange(function() {
+        line_visble = guiControls.line;
+        for (var i=0; i < lines.length; i++) {
+            lines[i].visible = guiControls.line;
+        }
+    });
+    folderVis.open(); 
+    
+    const folderArrow = gui.addFolder('view');
+    const ZoomControl = folderArrow.add(guiControls, 'zoom_factor', 0.1, 1.0);
+    ZoomControl.step(0.001).name('zoom').onChange(function() {
+        camera.zoom = guiControls.zoom_factor*20;
+        camera.updateProjectionMatrix();
+    }); 
+    // const arrowColorControl = folderArrow.addColor(guiControls, 'arrowColor').onChange(function() {
+    //     arrowHelper.setColor(guiControls.arrowColor);
+    // });
+    folderArrow.open(); 
+    
+    gui.add(guiControls, 'resetView').name('reset view');
+}
+
+
+window.addEventListener( 'mousemove', function(e) {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+    planeNormal.copy(camera.position).normalize();
+    plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+    raycaster.ray.intersectPlane(plane, planePoint);
+});
+
+window.addEventListener('dblclick', function(e) {
+    drawpoint();
+    if (vertices.length > 1) {
+        var lines_draw = [];
+        lines_draw.push(vertices[vertices.length-2]);
+        lines_draw.push(vertices[vertices.length-1]);
+        var line = drawline(lines_draw, 0xB4B4B3);
+        scene.add(line);
+        lines.push(line);
+
+        if (line_visble == false) {
+            for (var i=0; i < lines.length; i++) {
+                lines[i].visible = false;
+            }
+        }
+        
+        var curve = drawcurve(vertices);
+        scene.add(curve);
+        curves.push(curve);
+    }
+});
+
+// prevent right click menu
+window.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+    return false;
+});
+
+function drawpoint(){
+    vertices.push(new THREE.Vector3(planePoint.x,planePoint.y, planePoint.z));
+    var geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'index', new THREE.Uint16BufferAttribute(new THREE.Vector2(v_index), 1));
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( new THREE.Vector3(planePoint.x,planePoint.y, planePoint.z), 3 ) );
+    var material = new THREE.PointsMaterial( {color:0x952323, size: 30} );
+    const p = new THREE.Points( geometry, material );
+    scene.add( p );
+    points.push(p);
+    point_geometry = geometry;
+    point = p;
+
+    v_index += 1;
+
+    if (points.length > 0) {
+        var controls = new THREE.DragControls( points, camera, renderer.domElement );
+        controls.addEventListener( 'dragstart', dragStartCallback );
+        controls.addEventListener( 'dragend', dragendCallback );
+    }
+}
+
+function drawline(points, color){
+    var lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
+    var lineMaterial = new THREE.LineBasicMaterial( { color: color } );
+    var line = new THREE.Line( lineGeometry, lineMaterial );
+    return line;
+}
+
+
+function drawcurve(vertices){
+    for (var i=0; i < curves.length; i++) {
+        scene.remove(curves[i]);
+    }
+    var spline = new BSpline(vertices,3,true);
+    var oldx, oldy, x, y;
+    var curve_points = [];
+    oldx = spline.calcAt(0)[0];
+    oldy = spline.calcAt(0)[1];
+    curve_points.push(new THREE.Vector3(oldx, oldy, 0));
+    for(var t = 0;t <= 1;t+=0.001){
+        var interpol = spline.calcAt(t);
+        x = interpol[0];
+        y = interpol[1];
+        curve_points.push(new THREE.Vector3(x, y, 0));
+    }
+    var curve = drawline(curve_points, 0x1450A3);
+    return curve;
+}
+
+function dragStartCallback(event) {
+	startColor = 0x952323;
+	event.object.material.color.setHex(0xF4E869);
+    move_flag = true;
+}
+
+function dragendCallback(event) {
+    if (move_flag == true) {
+        event.object.material.color.setHex(startColor);
+        for (var i=0; i < lines.length; i++) {
+            scene.remove(lines[i]);
+        }
+
+        lines = [];
+
+        var newVertices = [];
+        for (var j = 0; j < vertices.length; j++) {
+            newVertices.push(vertices[j].clone()); // 각 요소를 복제하여 새로운 배열에 추가
+        }
+        newVertices[event.object.geometry.attributes.index.array[0]] = planePoint;
+
+        var line = drawline(newVertices, 0xB4B4B3);
+        scene.add(line);
+        lines.push(line);
+
+        if (line_visble == false) {
+            for (var i=0; i < lines.length; i++) {
+                lines[i].visible = false;
+            }
+        }
+
+        move_flag = false;
+
+        vertices = [];
+        for (var j = 0; j < newVertices.length; j++) {
+            vertices.push(newVertices[j].clone()); // 각 요소를 복제하여 새로운 배열에 추가
+        }
+
+        var curve = drawcurve(vertices);
+        scene.add(curve);
+        curves.push(curve);
+    }
+}
+
+animate();
